6e856b47cf4606f9ad40f5d5013ed2f7058b05b2
dannycahyo <dannydwicahyono@gmail.com>
2023-07-12 13:37:11 +0700

    chore: Setup react query and chakra UI for the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+  DehydratedState,
+} from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@src/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+
+export type MainProps = {
+  dehydratedState: DehydratedState;
+};
+
+export default function App({ Component, pageProps }: AppProps<MainProps>) {
+  const { dehydratedState } = pageProps;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
