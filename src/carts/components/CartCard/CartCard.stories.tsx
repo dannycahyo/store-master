ca2ec7987e39a2054c65a4ed0964aae9701d1bc6
dannycahyo <dannydwicahyono@gmail.com>
2023-07-13 16:43:12 +0700

    test: Implement testing on CartCard component

---
+++ src/carts/components/CartCard/CartCard.stories.tsx
@@ -0,0 +1,56 @@
+import { expect } from "@storybook/jest";
+import { Meta, StoryFn } from "@storybook/react";
+import { within } from "@storybook/testing-library";
+
+import { cartDetail } from "@src/constants/testData/cartDetail";
+import { CartCard } from "./CartCard";
+
+export default {
+  title: "Carts/Components/CartCard",
+  component: CartCard,
+} as Meta<typeof CartCard>;
+
+const Template: StoryFn<typeof CartCard> = (args) => <CartCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  cartDetail,
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const cartCard = canvas.getByRole("figure");
+  expect(cartCard).toBeInTheDocument();
+
+  const userIdLabel = canvas.getByText("User ID:");
+  expect(userIdLabel).toBeInTheDocument();
+  const userIdValue = canvas.getByText(cartDetail.userId.toString());
+  expect(userIdValue).toBeInTheDocument();
+
+  const discountedTotalLabel = canvas.getByText("Discounted Total:");
+  expect(discountedTotalLabel).toBeInTheDocument();
+  const discountedTotalValue = canvas.getByText(
+    cartDetail.discountedTotal.toString(),
+  );
+  expect(discountedTotalValue).toBeInTheDocument();
+
+  const totalLabel = canvas.getByText("Total:");
+  expect(totalLabel).toBeInTheDocument();
+  const totalValue = canvas.getByText(cartDetail.total.toString());
+  expect(totalValue).toBeInTheDocument();
+
+  const totalProductsLabel = canvas.getByText("Total Products:");
+  expect(totalProductsLabel).toBeInTheDocument();
+  const totalProductsValue = canvas.getByText(
+    cartDetail.totalProducts.toString(),
+  );
+  expect(totalProductsValue).toBeInTheDocument();
+
+  const totalQuantityLabel = canvas.getByText("Total Quantity:");
+  expect(totalQuantityLabel).toBeInTheDocument();
+  const totalQuantityValue = canvas.getByText(
+    cartDetail.totalQuantity.toString(),
+  );
+  expect(totalQuantityValue).toBeInTheDocument();
+};
