4028a6fb77ac68da346652389d84dee30df818bf
dannycahyo <dannydwicahyono@gmail.com>
2023-07-14 21:57:33 +0700

    feat: Implement API proxy for the get all categories

---
+++ src/pages/api/products/categories/index.ts
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/categories`,
+  );
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
