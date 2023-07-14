e5320b199d238898caa82489d71b491619a39bc6
dannycahyo <dannydwicahyono@gmail.com>
2023-07-14 17:46:07 +0700

    style: Adjust the width of the skeleton component to full

--- src/uikits/components/CardSkeleton/CardSkeleton.tsx
+++ src/uikits/components/CardSkeleton/CardSkeleton.tsx
@@ -4,7 +4,7 @@ import type React from "react";
 
 const CardSkeleton: React.FC = () => {
   return (
-    <Box padding="6" boxShadow="lg" bg="purple.100">
+    <Box padding="6" boxShadow="lg" bg="purple.100" w="full">
       <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="2" />
     </Box>
   );
--- src/uikits/components/TableSkeleton/TableSkeleton.tsx
+++ src/uikits/components/TableSkeleton/TableSkeleton.tsx
@@ -4,7 +4,7 @@ import type React from "react";
 
 const TableSkeleton: React.FC = () => {
   return (
-    <Stack role="progressbar">
+    <Stack role="progressbar" w="full">
       <Skeleton startColor="purple.500" endColor="blue.500" height={20} />
       <Skeleton startColor="purple.500" endColor="blue.500" height={20} />
       <Skeleton startColor="purple.500" endColor="blue.500" height={20} />
