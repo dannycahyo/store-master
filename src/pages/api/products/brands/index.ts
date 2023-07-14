31f33478d8c9697143ef89d8f2dae1d75cbe9b7d
dannycahyo <dannydwicahyono@gmail.com>
2023-07-14 22:05:47 +0700

    feat: Implement API proxy for get all brand

---
+++ src/pages/api/products/brands/index.ts
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+
+import type { Product } from "@src/products/model";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req_: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
+  const data = await response.json();
+
+  const brands = [
+    ...new Set<Product[]>(
+      data.products.map((product: Product) => product.brand),
+    ),
+  ];
+
+  res.status(200).json(brands);
+}
