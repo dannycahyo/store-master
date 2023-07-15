399266fe759ab1b9dd27224adc08eecf2736c974
dannycahyo <dannydwicahyono@gmail.com>
2023-07-15 08:00:13 +0700

    feat: Add price range filter for the get product by category service

--- src/pages/api/products/category/[category]/index.ts
+++ src/pages/api/products/category/[category]/index.ts
@@ -1,23 +1,52 @@
 import fetch from "node-fetch";
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { Product, ProductsResponse } from "@src/products/model";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { category, limit, skip, select } = req.query;
+  const { limit = "0", skip = "0", select, category, pMax, pMin } = req.query;
 
   const url = new URL(
     `${process.env.NEXT_PUBLIC_API_URL}/products/category/${category}`,
   );
 
-  if (limit) url.searchParams.set("limit", limit as string);
-  if (skip) url.searchParams.set("skip", skip as string);
-  if (select) url.searchParams.set("select", select as string);
+  url.searchParams.set("limit", "0");
 
   const response = await fetch(url);
-  const data = await response.json();
+  const data: ProductsResponse = await response.json();
 
-  res.status(200).json(data);
+  let paginatedProducts =
+    limit !== "0"
+      ? data.products.slice(Number(skip), Number(skip) + Number(limit))
+      : data.products.slice(Number(skip));
+
+  if (pMin && pMax) {
+    paginatedProducts = paginatedProducts.filter(
+      (product: Product) =>
+        product.price >= Number(pMin) && product.price <= Number(pMax),
+    );
+  }
+
+  if (select) {
+    const defaultSelectedField = "id,";
+    const selectFields = ((defaultSelectedField + select) as string).split(",");
+    paginatedProducts = paginatedProducts.map((product: Product) => {
+      const selectedProductMap = new Map();
+      selectFields.forEach((field: string) => {
+        selectedProductMap.set(field, product[field as keyof Product]);
+      });
+      const selectedProduct = Object.fromEntries(selectedProductMap);
+      return selectedProduct as Product;
+    });
+  }
+
+  res.status(200).json({
+    products: paginatedProducts,
+    total: paginatedProducts.length,
+    skip: Number(skip),
+    limit: Number(limit),
+  });
 }
