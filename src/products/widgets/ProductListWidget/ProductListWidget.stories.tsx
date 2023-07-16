adf59bf5abed25f7fadae444f64c5eee822d2f4d
dannycahyo <dannydwicahyono@gmail.com>
2023-07-16 20:05:46 +0700

    test: Implement testing for the error case of ProductListWidget component

--- src/products/widgets/ProductListWidget/ProductListWidget.stories.tsx
+++ src/products/widgets/ProductListWidget/ProductListWidget.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryFn } from "@storybook/react";
+import { waitForElementToBeRemoved, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 import { ProductListWidget } from "./ProductListWidget";
 import {
@@ -44,4 +46,44 @@ ProductListWidgetError.parameters = {
     ...errorProductCategoriesHandler,
     ...errorProductsHandler,
   ],
+};
+
+ProductListWidgetError.play = async ({ canvasElement, step }) => {
+  const canvas = within(canvasElement);
+  await step("wait for loading to be finished", async () => {
+    await waitForElementToBeRemoved(
+      () => canvas.queryAllByRole("progressbar"),
+      {
+        timeout: 30000,
+      },
+    );
+  });
+
+  await step("user see the alert element", async () => {
+    const alertElement = canvas.getAllByRole("alert")[0];
+    expect(alertElement).toBeInTheDocument();
+  });
+
+  await step(
+    "user see the error message of the categories & brands",
+    async () => {
+      const alertTitle = canvas.getByText(/Categories & Brands Error!/i);
+      expect(alertTitle).toBeInTheDocument();
+
+      const alertDescription = canvas.getByText(
+        /There was something wrong when fetching get all categories & brands./i,
+      );
+      expect(alertDescription).toBeInTheDocument();
+    },
+  );
+
+  await step("user see the error message of the products", async () => {
+    const alertTitle = canvas.getByText(/Products Error!/i);
+    expect(alertTitle).toBeInTheDocument();
+
+    const alertDescription = canvas.getByText(
+      /There was something wrong when fetching get all products./i,
+    );
+    expect(alertDescription).toBeInTheDocument();
+  });
 };
