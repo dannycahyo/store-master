0ac7e61d99eab6e6a8863de3a476b9fda7e94f5b
dannycahyo <dannydwicahyono@gmail.com>
2023-07-17 19:13:24 +0700

    feat: Implement hamburger menu on the Container mobile view

--- src/uikits/components/Container/Container.tsx
+++ src/uikits/components/Container/Container.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import {
   Grid,
   GridItem,
   Container as ChakraContainer,
   useMediaQuery,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
 } from "@chakra-ui/react";
+import { match } from "ts-pattern";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { menuItems } from "@src/constants";
 
 import type React from "react";
 
@@ -19,15 +28,39 @@ const Container: React.FC<ContainerProps> = ({ sidebar, children }) => {
     <>
       {sidebar !== undefined ? (
         <Grid minH="100vh" templateColumns="repeat(5, 1fr)" gap={4}>
-          {isBelowTabletSize ? null : (
-            <GridItem colSpan={1}>{sidebar}</GridItem>
-          )}
+          {match(isBelowTabletSize)
+            .with(true, () => null)
+            .with(false, () => <GridItem colSpan={1}>{sidebar}</GridItem>)
+            .exhaustive()}
           <GridItem
             colSpan={{ base: 5, md: 5, lg: 4 }}
             pr={4}
             pl={{ base: 4, lg: 0 }}
             py={8}
           >
+            {match(isBelowTabletSize)
+              .with(true, () => (
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    aria-label="Options"
+                    icon={<HamburgerIcon />}
+                    variant="outline"
+                    mb={4}
+                  />
+                  <MenuList>
+                    {menuItems.map((menuItem) => (
+                      <MenuItem key={menuItem}>
+                        <Link href={`/${menuItem.toLowerCase()}`}>
+                          {menuItem}
+                        </Link>
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </Menu>
+              ))
+              .with(false, () => null)
+              .exhaustive()}
             {children}
           </GridItem>
         </Grid>
